c7b0a5b98ee63a8dc55c0a8a817c5a992a76e888
Iridiscencia Álvarez <airidiscencia4@proton.me>
2023-12-13 09:38:20 -0300

    Added persistency

--- src/gui.py
+++ src/gui.py
@@ -1,9 +1,12 @@
 import csv as CSV
 import dataclasses as DC
+import json as JSON
 import pathlib as PTH
 import sys as SYS
 import typing as TP
 
+import PySide6.QtCore as QTC
+import PySide6.QtGui as QTG
 import PySide6.QtWidgets as QTW
 
 from wows_io import WowsIo
@@ -41,7 +44,7 @@ class FileSelectorWidget(QTW.QWidget):
         self._button = QTW.QPushButton()
         self._button.setText("Select")
         self._button.clicked.connect(self._select)
-        layout.addWidget(self._button)
+        layout.addWidget(self._button, 0)
 
         self._label = QTW.QLabel()
         self._label.setText("/".join(self.value.rsplit("/", 2)[1:]))
@@ -76,16 +79,20 @@ class ChangesWidget(QTW.QWidget):
 
         self.setLayout(layout)
 
-    def add_row(self) -> None:
+    def add_row(self, commander: str = "", name: str = "", portrait: str = "",
+                voice_over: str = "") -> None:
         layout = self.layout()
         row = layout.rowCount()
         commander_input = QTW.QComboBox()
         commander_input.addItems(self._commanders.keys())
+        commander_input.setCurrentText(commander)
         name_input = QTW.QLineEdit()
-        portrait_input = FileSelectorWidget(self._default_portrait_dir)
+        name_input.setText(name)
+        portrait_input = FileSelectorWidget(self._default_portrait_dir, portrait)
         voice_over_input = QTW.QComboBox()
         voice_over_input.addItem(_none_sentinel)
         voice_over_input.addItems(self._voice_overs)
+        voice_over_input.setCurrentText(voice_over)
         remove_button = QTW.QPushButton()
         remove_button.setText("Remove")
         remove_button.clicked.connect(lambda: self._remove_row(row))
@@ -118,16 +125,31 @@ class ChangesWidget(QTW.QWidget):
                     name_changes[recipient.name_id] = name
                 if portrait != "":
                     portrait_changes[recipient.portrait_path] = PTH.Path(portrait)
-                if voice_over != _none_sentinel:
+                if voice_over not in [_none_sentinel, ""]:
                     voice_changes[recipient.voice_id] = voice_over
         return Mod(name_changes, portrait_changes, voice_changes)
+
+    def get_changes_as_json(self) -> TP.List[TP.Dict[str, str]]:
+        changes = []
+        layout = self.layout()
+        for row in range(1, layout.rowCount()):
+            if layout.itemAtPosition(row, 0) is not None:
+                changes.append({
+                    "commander": layout.itemAtPosition(row, 0).widget().currentText(),
+                    "name": layout.itemAtPosition(row, 1).widget().text(),
+                    "portrait": layout.itemAtPosition(row, 2).widget().value,
+                    "voice_over": layout.itemAtPosition(row, 3).widget().currentText()
+                })
+        return changes
+
 
 
 class MainWidget(QTW.QWidget):
 
     def __init__(self, commanders: TP.Dict[str, RecipientCommander],
                  languages: TP.List[str], default_portrait_dir: PTH.Path,
-                 voice_overs: TP.List[str], io: WowsIo):
+                 voice_overs: TP.List[str], io: WowsIo,
+                 session: TP.Optional[dict] = None):
         super().__init__()
         self._io = io
         main_layout = QTW.QVBoxLayout()
@@ -137,19 +159,29 @@ class MainWidget(QTW.QWidget):
         main_layout.addWidget(self._changes_widget)
 
         form_layout = QTW.QFormLayout()
+        form_layout.setLabelAlignment(QTC.Qt.AlignmentFlag.AlignRight)
 
         self._language_widget = QTW.QComboBox()
         self._language_widget.addItems(languages)
-        self._language_widget.setCurrentText("en")
         form_layout.addRow("Language:", self._language_widget)
 
         self._voice_mod_name_widget = QTW.QLineEdit()
-        self._voice_mod_name_widget.setText("My Voice Mod")
         form_layout.addRow("Voice mod name:", self._voice_mod_name_widget)
 
         self._voice_mod_id_widget = QTW.QLineEdit()
-        self._voice_mod_id_widget.setText("wcar")
         form_layout.addRow("Voice mod ID:", self._voice_mod_id_widget)
+
+        if session is None:
+            self._language_widget.setCurrentText("en")
+            self._voice_mod_name_widget.setText("My Voice Mod")
+            self._voice_mod_id_widget.setText("wcar")
+        else:
+            self._language_widget.setCurrentText(session["language"])
+            self._voice_mod_name_widget.setText(session["voice_mod_name"])
+            self._voice_mod_id_widget.setText(session["voice_mod_id"])
+            for change in session["changes"]:
+                self._changes_widget.add_row(change["commander"], change["name"],
+                                             change["portrait"], change["voice_over"])
 
         main_layout.addLayout(form_layout)
 
@@ -170,8 +202,23 @@ class MainWidget(QTW.QWidget):
         io.install_voice_overs(changes.voice_changes,
                                self._voice_mod_name_widget.text(),
                                self._voice_mod_id_widget.text())
+        self._save_session()
         self._install_button.setText("Success!")
         print("Success!")
+
+    def _save_session(self) -> None:
+        print("Saving session.")
+        with open("session.json", "w") as session_file:
+            session_file.write(JSON.dumps({
+                "language": self._language_widget.currentText(),
+                "voice_mod_name": self._voice_mod_name_widget.text(),
+                "voice_mod_id": self._voice_mod_id_widget.text(),
+                "changes": self._changes_widget.get_changes_as_json()
+            }, indent=2))
+
+    def closeEvent(self, event: QTG.QCloseEvent) -> None:
+        self._save_session()
+        super().closeEvent(event)
 
 
 def main() -> None:
@@ -190,10 +237,16 @@ def main() -> None:
     working_dir = PTH.Path("working")
     io = WowsIo(wows_dir, output_dir, working_dir)
 
+    try:
+        with open("session.json") as session_file:
+            session = JSON.load(session_file)
+    except FileNotFoundError:
+        session = None
+
     with io:
         window = MainWidget(commanders, io.list_languages(),
                             working_dir/"gui"/"crew_commander"/"base",
-                            io.list_voice_overs(), io)
+                            io.list_voice_overs(), io, session)
         window.setWindowTitle(title)
         window.show()
         app.exec()
